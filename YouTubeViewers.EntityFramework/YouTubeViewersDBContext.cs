41c2437a5482a53412b61ccbb6079398618cf70e
Erroman <vladimir_roman@mail.ru>
2024-02-14 01:11:03 +0500

    DTOs objects established

---
+++ YouTubeViewers.EntityFramework/YouTubeViewersDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YouTubeViewers.Domain.Models;
+using YouTubeViewers.EntityFramework.DTOs;
+
+namespace YouTubeViewers.EntityFramework
+{
+    public class YouTubeViewersDBContext : DbContext
+    {
+        public DbSet<YouTubeViewerDto> YouTubeViewers { get; set; }
+    }
+}
