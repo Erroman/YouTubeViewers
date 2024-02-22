1256c55ec29074074527c43475f00c3c975e8171
Erroman <vladimir_roman@mail.ru>
2024-02-22 09:56:00 +0500

    All CRUD commands implemented

--- YouTubeViewers.EntityFramework/Commands/DeleteYouTubeViewerCommand.cs
+++ YouTubeViewers.EntityFramework/Commands/DeleteYouTubeViewerCommand.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YouTubeViewers.Domain.Commands;
+using YouTubeViewers.Domain.Models;
+using YouTubeViewers.EntityFramework.DTOs;
 
 namespace YouTubeViewers.EntityFramework.Commands
 {
-    public  class DeleteYouTubeViewerCommand
+    public  class DeleteYouTubeViewerCommand : IDeleteYouTubeViewerCommand
     {
         private readonly YouTubeViewersDBContextFactory _contextFactory;
 
-        public GetAllYouTubeViewersQuery(YouTubeViewersDBContextFactory contextFactory)
+        public DeleteYouTubeViewerCommand(YouTubeViewersDBContextFactory contextFactory)
         {
             _contextFactory = contextFactory;
+        }
+
+        public async Task Execute(Guid id)
+        {
+                using (YouTubeViewersDBContext context = _contextFactory.Create())
+                {
+                    YouTubeViewerDto youTubeViewersDtos = new YouTubeViewerDto()
+                    {
+                        Id = id,
+                    };
+                    context.YouTubeViewers.Remove(youTubeViewersDtos);
+                    await context.SaveChangesAsync();
+
+                }
         }
     }
 }
--- YouTubeViewers.EntityFramework/Commands/UpdateYouTubeViewerCommand.cs
+++ YouTubeViewers.EntityFramework/Commands/UpdateYouTubeViewerCommand.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YouTubeViewers.Domain.Commands;
+using YouTubeViewers.Domain.Models;
+using YouTubeViewers.EntityFramework.DTOs;
 
 namespace YouTubeViewers.EntityFramework.Commands
 {
-    public class UpdateYouTubeViewerCommand
+    public class UpdateYouTubeViewerCommand : IUpdateYouTubeViewerCommand
     {
         private readonly YouTubeViewersDBContextFactory _contextFactory;
 
-        public GetAllYouTubeViewersQuery(YouTubeViewersDBContextFactory contextFactory)
+        public UpdateYouTubeViewerCommand(YouTubeViewersDBContextFactory contextFactory)
         {
             _contextFactory = contextFactory;
         }
+
+        public async Task Execute(YouTubeViewer youTubeViewer)
+        {
+            using (YouTubeViewersDBContext context = _contextFactory.Create())
+            {
+                YouTubeViewerDto youTubeViewersDtos = new YouTubeViewerDto()
+                {
+                    Id = youTubeViewer.Id,
+                    Username = youTubeViewer.Username,
+                    IsSubscribed = youTubeViewer.IsSubscribed,
+                    IsMember = youTubeViewer.IsMember
+                };
+                context.YouTubeViewers.Update(youTubeViewersDtos);
+                await context.SaveChangesAsync();
+
+            }
+
+
+        }
+    }
+}
     }
 }
